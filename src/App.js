a0e0a15e705511e247a7d4ec7bf4b442e295f16b
SintFD <sintfd@gmail.com>
2022-05-26 18:12:35 +0400

    ready template

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Route } from "react-router-dom";
+
+import "./reset.css";
+
+function App() {
+  return (
+    <div className="app">
+      <Route exact path="/">
+        
+      </Route>
+    </div>
+  );
+}
+
+export default App;
